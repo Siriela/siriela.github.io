d09921b35e8171962b4bb79f124dbbe14a9cac34
Karin Toft <karin.toft@axesslab.com>
2023-04-05 23:40:11 +0200

    spara resultat, skriv ut tid efter klart

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ const onBeforeUnload = (e) => {
 };
 
 const onLoad = () => {
+    console.log(running);
+    document.getElementById('stop').innerText = labelStartPauseButton();
     const oldTime = parseInt(localStorage.getItem('timestampLeave'));
     const now = Date.now();
     timeDiff = oldTime ? Math.floor((now - oldTime) / 1000) : 0;
@@ -37,13 +39,16 @@ window.addEventListener('visibilitychange', onVisibilityChange);
 
 function start(){ 
     document.getElementById('wrapper').classList.add('pulse');
-    running = true;  
+     
     if (localStorage.getItem('timer')) {seconds = localStorage.getItem('timer') - timeDiff;}
     else {
         seconds = parseInt(document.getElementById('set-hours').value) * 3600 + parseInt(document.getElementById('set-minutes').value) * 60 + parseInt(document.getElementById('set-seconds').value);
     }
-    timer = setInterval(startTimer, 1000);
+    timer = setInterval(runTimer, 1000);
+    running = true; 
     closeModal();
+    document.getElementById('stop').innerText = labelStartPauseButton();
+    document.getElementById('chosen-time').innerText = parseTime();
 }
 
 function reset() {
@@ -63,17 +68,21 @@ function openDialog() {
     document.getElementById('overlay').setAttribute('style', 'display: block');
 }
 
-function stopp() {
-    document.getElementById('wrapper').classList.remove('pulse');
-
-    running = false;
-    clearInterval(timer);
-    localStorage.setItem('timer', seconds);
+const labelStartPauseButton = () => {
+    return running ? 'Pausa' : 'Starta';
 }
 
-function resume() {
-    if (!running) {
+function pause() {
+    if (running) {
+        document.getElementById('wrapper').classList.remove('pulse');
+        clearInterval(timer);
+        localStorage.setItem('timer', seconds);
+        document.getElementById('stop').innerText = 'Starta';
+        running = false;
+    }
+    else {
         start();
+        document.getElementById('stop').innerText = 'Pausa';
     }
 }
 
@@ -88,16 +97,28 @@ function sploink () {
 	sploink.play();
 }
 
-function startTimer() {   
+function parseTime() {
     const minutes = seconds / 60;
     const hours = minutes / 60;
     
-    displaySeconds = seconds % 60 < 10 ? '0' + Math.floor(seconds % 60) : Math.floor(seconds % 60);
-    displayMinutes = minutes % 60 < 10 ? '0' + Math.floor(minutes % 60) : Math.floor(minutes % 60);
-    displayHours = Math.floor(hours) < 10 ? '0' + Math.floor(hours) : Math.floor(hours);
-    
-    const secondsToTime = displayHours + ':' + displayMinutes + ':' + displaySeconds;
-    document.getElementById('time').innerHTML = seconds > 0 ? secondsToTime : seconds.toString().replace('-', '');
+    displaySecondsPos = seconds % 60 < 10 ? '0' + Math.floor(seconds % 60) : Math.floor(seconds % 60);
+    displaySecondsNeg = seconds % 60 > -10 ? '0' + Math.ceil(seconds % 60).toString().replace('-', '') : Math.ceil(seconds % 60).toString().replace('-', '');
+
+    displayMinutesPos = minutes % 60 < 10 ? '0' + Math.floor(minutes % 60) : Math.floor(minutes % 60);
+    displayMinutesNeg = minutes % 60 > -10 ? '0' + Math.ceil(minutes % 60).toString().replace('-', '') : Math.floor(minutes % 60).toString().replace('-', '');
+
+    displayHoursPos = Math.floor(hours) < 10 ? '0' + Math.floor(hours) : Math.floor(hours);
+    displayHoursNeg = Math.ceil(hours) > -10 ? '0' + Math.ceil(hours).toString().replace('-', '') : Math.ceil(hours).toString().replace('-', '');
+
+    const secondsToTimePos = '-' + displayHoursPos + ':' + displayMinutesPos + ':' + displaySecondsPos;
+    const secondsToTimeNeg = displayHoursNeg + ':' + displayMinutesNeg + ':' + displaySecondsNeg;
+
+    return seconds > 0 ? secondsToTimePos : secondsToTimeNeg;
+}
+
+function runTimer() {   
+    parseTime();    
+    document.getElementById('time').innerHTML = parseTime();
     
     seconds === 0 && timesUp();
     seconds--;   
@@ -110,4 +131,11 @@ function stopTimer(timer) {
 function closeModal() {
     document.getElementById('count-dialog').removeAttribute('open');
     document.getElementById('overlay').removeAttribute('style');
+}
+
+function save(value) {
+    console.log('value ', value);
+    document.getElementById('result').innerText = value;
+    stopTimer(timer);
+    reset();
 }
